37a43bd02fd1aa4a220f8dd15c53c7277110016b
Renat <60031371+NiXbi-L@users.noreply.github.com>
2024-10-07 21:09:55 +1000

    Bot

---
+++ main.py
@@ -0,0 +1,51 @@
+import logging
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from aiogram import Bot, Dispatcher, types
+import asyncio
+
+# Настройки
+API_TOKEN = '7719954043:AAFu8-nkq29EAuvKoiY1kuARNwqspu9dTEo'
+GOOGLE_SHEETS_CREDENTIALS_FILE = 'deliverysaves-702cc766d001.json'
+SPREADSHEET_ID = '1YKI0CMHQ22rqEdTzsV0lAIWYl2j1b1fmVUC-Z4ZKMpM'
+SHEET_NAME = 'S'
+TOPIC_NAME = 2
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+# Инициализация Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEETS_CREDENTIALS_FILE, scope)
+client = gspread.authorize(credentials)
+sheet = client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
+
+
+# Обработчик новых сообщений
+@dp.message()
+async def save_message(message: types.Message):
+    print(message.message_thread_id)
+    print(message.text)
+    try:
+        if message.is_topic_message and message.message_thread_id is not None:
+            topic_name = message.message_thread_id  # Здесь нужно получить имя топика, если это возможно
+            if topic_name == TOPIC_NAME:
+                user_first_name = message.from_user.first_name
+                user_message = message.text
+
+                # Сохранение в Google Sheets
+                sheet.append_row([user_first_name, user_message])
+
+                logging.info(f"Сохранено: {user_first_name} - {user_message}")
+    except:
+        print('Иди нахуй')
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
